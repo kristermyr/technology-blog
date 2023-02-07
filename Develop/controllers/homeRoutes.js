a6b508ef7080bc765c798969a1cad947182f6129
kristermyr <krister90@gmail.com>
2023-02-07 15:37:25 -0700

    working on getting logout button to work

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -30,15 +30,26 @@ router.get("/dashboard", (req, res) => {
 
   res.render("login");
 });
-router.get("/profile", (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.loggedIn) {
-    res.render("dashboard");
-    return;
-  }
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
 
-  res.render("login");
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 
 
