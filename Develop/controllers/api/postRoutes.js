2985b66c4e8bb1c76d5e88472718349f3e599406
kristermyr <110065764+kristermyr@users.noreply.github.com>
2023-02-22 13:37:53 -0700

    complete, starting readme

--- Develop/controllers/api/postRoutes.js
+++ Develop/controllers/api/postRoutes.js
@@ -19,28 +19,7 @@ router.post("/",withAuth, async (req,res) => {
         })
         })
 
-        router.get('/dashboard', withAuth, async (req, res) => {
-            try {
-                const userData = await User.findByPk(req.session.user_id, {
-                    attributes: {
-                        exclude: ['password']
-                    },
-                    include: [{
-                        model: Post
-                    }],
-                });
         
-                const user = userData.get({
-                    plain: true
-                });
         
-                res.render('dashboard', {
-                    ...user,
-                    logged_in: true
-                });
-            } catch (err) {
-                res.status(500).json(err);
-            }
-        });
 
 module.exports = router;
