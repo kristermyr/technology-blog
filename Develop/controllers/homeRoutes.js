d53cd2829df70728190991f3a0a138c62b654069
kristermyr <110065764+kristermyr@users.noreply.github.com>
2023-02-22 13:16:55 -0700

    comments fixed

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -2,13 +2,6 @@ const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
-// router.get("/", async (req, res) => {
-//   res.render("homepage", {loggedIn:req.session.loggedIn});
-// });
-// router.get("/dashboard", async (req, res) => {
-//   res.render("dashboard", { loggedIn: req.session.loggedIn });
-// });
-
 router.get("/dashboard/newpost", withAuth, async (req, res) => {
   res.render("newpost");
 });
