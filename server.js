eec9839635fa85e17416af85c451335beeb8d5f4
kristermyr <110065764+kristermyr@users.noreply.github.com>
2023-02-23 17:00:54 -0700

    heroku

--- server.js
+++ server.js
@@ -40,14 +40,13 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
 
-app.use(function (req, res, next) {
-  res.locals.session = req.session;
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.locals.session = req.session;
+//   next();
+// });
